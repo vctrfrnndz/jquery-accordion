36f22ec8ec9529fbbfe25400b8d396087f7442da
Victor Fernández <vic@Victors-iMac.local>
2014-07-27 14:20:58 -0300

    Small code refactor as suggested by @aaronldunlap

--- js/jquery.accordion.js
+++ js/jquery.accordion.js
@@ -68,7 +68,7 @@
 
             var v = ['Moz', 'webkit', 'Webkit', 'Khtml', 'O', 'ms'];
 
-            p = p.charAt(0).toUpperCase() + p.substr(1);
+            p = 'Transition';
 
             for (var i=0; i<v.length; i++) {
                 if (typeof s[v[i] + p] == 'string') {
